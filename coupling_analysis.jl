69f3deddbf6d8c9a51a70a4fbf09a168df708804
Charlotte Ward <cward@uoguelph.ca>
2021-12-09 17:34:07 -0500

    added new correct model

--- coupling_analysis.jl
+++ coupling_analysis.jl
@@ -9,44 +9,48 @@ pygui(true)
 
 @with_kw mutable struct Par 
     
-    r_litt = 2.7
-    r_pel = 2.7
-    α_pel = 0.5   ##competitive influence of pelagic resource on littoral resource 
-    α_litt = 0.5   ## competitve influence of littoral resource on pelagic resource
-    K = 2.5
-    h_CR = 0.5
-    h_PC = 0.5
-    h_PR = 0.5
-    e_CR = 0.5
-    e_PC = 0.5
-    e_PR = 0.5
-    m_C = 0.5
-    m_P = 0.2
-    a_CR_litt = 1.5
-    a_CR_pel = 1.5
+    r_litt = 1.5
+    r_pel = 1.5
+    α_pel = 0.0  ##competitive influence of pelagic resource on littoral resource 
+    α_litt = 0.0   ## competitve influence of littoral resource on pelagic resource
+    K = 1.5
+    h_CR = 0.8
+    h_PC = 0.6
+    h_PR = 0.9
+    e_CR = 0.7
+    e_PC = 0.6
+    e_PR = 0.4
+    m_C = 0.3
+    m_P = 0.3
+    a_CR_litt = 1.0
+    a_CR_pel = 1.0
     a_PR_litt = 0
     a_PR_pel = 0
-    a_PC_litt = 0.5
-    a_PC_pel = 1.8
-    noise = 0.1
+    a_PC_litt = 1.0
+    a_PC_pel = 1.0
+    Σ = 0.0
+    
     
 end
 
 function coupling_model!(du, u, p, t)
-    @unpack r_litt, r_pel, K, α_pel, α_litt, e_CR, e_PC, e_PR,  a_CR_litt, a_CR_pel, a_PR_litt, a_PR_pel, h_CR, h_PC, h_PR, m_C, m_P, a_PC_litt, a_PC_pel= p 
+
+    @unpack r_litt, r_pel, K, α_pel, α_litt, e_CR, e_PC, e_PR,  a_CR_litt, a_CR_pel, a_PR_litt, a_PR_pel, h_CR, h_PC, h_PR, m_C, m_P, a_PC_litt, a_PC_pel, Σ = p 
     
     
     R_l, R_p, C_l, C_p, P = u
-    
-    du[1]= r_litt * R_l * (1 - (α_pel * R_p + R_l)/K) - (a_CR_litt * R_l * C_l)/(1 + a_CR_litt * h_CR * R_l) - (a_PR_litt * R_l * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt * h_PC * C_l + a_PC_pel * h_PC * C_p)
-    
-    du[2] = r_pel * R_p * (1 - (α_litt * R_l + R_p)/K) - (a_CR_pel * R_p * C_p)/(1 + a_CR_pel * h_CR * R_p) - (a_PR_pel * R_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt * h_PC * C_l + a_PC_pel * h_PC * C_p)
 
-    du[3] = (e_CR * a_CR_litt * R_l * C_l)/(1 + a_CR_litt * h_CR * R_l) - (a_PC_litt * C_l * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt * h_PC * C_l + a_PC_pel * h_PC * C_p) - m_C * C_l
+    Ω = (Σ * C_p)/(Σ * C_p + (1-Σ)*C_l)
     
-    du[4] = (e_CR * a_CR_pel * R_p * C_p)/(1 + a_PC_pel * h_PC * R_p) - (a_PC_pel * C_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt * h_PC * C_l + a_PC_pel * h_PC * C_p) - m_C * C_p
+    du[1]= r_litt * R_l * (1 - (α_pel * R_p + R_l)/K) - (a_CR_litt * R_l * C_l)/(1 + a_CR_litt * h_CR * R_l) - (a_PR_litt * R_l * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt*(1-Ω) * h_PC * C_l + a_PC_pel * Ω * h_PC * C_p)
     
-    du[5] = (e_PR * a_PR_litt * R_l * P + e_PR * a_PR_pel * R_p * P + e_PC * a_PC_litt * C_l * P + e_PC * a_PC_pel * C_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt * h_PC * C_l + a_PC_pel * h_PC * C_p) - m_P * P
+    du[2] = r_pel * R_p * (1 - (α_litt * R_l + R_p)/K) - (a_CR_pel * R_p * C_p)/(1 + a_CR_pel * h_CR * R_p) - (a_PR_pel * R_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt* (1-Ω) * h_PC * C_l + a_PC_pel* Ω  * h_PC * C_p)
+
+    du[3] = (e_CR * a_CR_litt * R_l * C_l)/(1 + a_CR_litt * h_CR * R_l) - (a_PC_litt* (1-Ω)  * C_l * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt* (1-Ω)  * h_PC * C_l + a_PC_pel* Ω  * h_PC * C_p) - m_C * C_l
+    
+    du[4] = (e_CR * a_CR_pel * R_p * C_p)/(1 + a_PC_pel* Ω  * h_PC * R_p) - (a_PC_pel* Ω  * C_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt* (1-Ω)  * h_PC * C_l + a_PC_pel* Ω  * h_PC * C_p) - m_C * C_p
+    
+    du[5] = (e_PR * a_PR_litt * R_l * P + e_PR * a_PR_pel * R_p * P + e_PC * a_PC_litt * (1-Ω)  * C_l * P + e_PC * a_PC_pel* Ω  * C_p * P)/(1 + a_PR_litt * h_PR * R_l + a_PR_pel * h_PR * R_p + a_PC_litt* (1-Ω)  * h_PC * C_l + a_PC_pel * Ω  * h_PC * C_p) - m_P * P
 
     return 
 end
@@ -70,9 +74,9 @@ let
     u0 = [0.5, 0.5, 0.3, 0.3, 0.3]
     t_span = (0, 2000.0)
     ts =range(9500, 10000, length = 500)
-    p = Par(a_PC_litt= 0.0)
+    p = Par(Σ = 0.0)
     prob = ODEProblem(coupling_model!, u0, t_span, p)
-    sol = solve(prob, reltol = 1e-8, atol = 1e-8)
+    sol = solve(prob)
     grid = sol(ts)
     model_ts = figure()
     PyPlot.plot(sol.t, sol.u)
@@ -87,12 +91,12 @@ end
 ## equilibrium check for parameter range -- find where all species coexist (interior equilibrium)
 
 
-coup_vals = 0.0:0.05:1.75
+coup_vals = 0.0:0.005:0.74
 coup_hold = fill(0.0,length(coup_vals),6)
 
 u0 = [0.5,0.5, 0.3, 0.3, 0.3]
 t_span = (0, 10000.0)
-p = Par()
+p = Par(Σ = 0.0)
 ts =range(9500, 10000, length = 500)
 prob = ODEProblem(coupling_model!, u0, t_span, p)
 sol = solve(prob, reltol = 1e-8, atol = 1e-8)
@@ -100,7 +104,7 @@ grid = sol(ts)
 eq = nlsolve((du, u) -> coupling_model!(du, u, p, 0.0), grid.u[end]).zero
 
 for i=1:length(coup_vals)
-    p = Par(a_PC_litt=coup_vals[i])
+    p = Par(Σ =coup_vals[i])
     if i==1
         u0 = [0.5, 0.5, 0.3, 0.3, 0.3]
        
@@ -153,15 +157,15 @@ for i=1:length(coup_vals)
 end
 
 
-eig_1 = Plots.plot(eig_hold[:,1], eig_hold[:,2], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Omnivory ", ylabel = " Eig 1 " )
+eig_1 = Plots.plot(eig_hold[:,1], eig_hold[:,2], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Coupling ", ylabel = " Eig 1 " )
 
-eig_2 = Plots.plot(eig_hold[:,1], eig_hold[:,3], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Omnivory ", ylabel = " Eig 2 " )
+eig_2 = Plots.plot(eig_hold[:,1], eig_hold[:,3], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Coupling ", ylabel = " Eig 2 " )
 
-eig_3 = Plots.plot(eig_hold[:,1], eig_hold[:,4], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Omnivory ", ylabel = " Eig 3 " )
+eig_3 = Plots.plot(eig_hold[:,1], eig_hold[:,4], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Coupling ", ylabel = " Eig 3 " )
 
-eig_4 = Plots.plot(eig_hold[:,1], eig_hold[:,5], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Omnivory ", ylabel = " Eig 4 " )
+eig_4 = Plots.plot(eig_hold[:,1], eig_hold[:,5], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Coupling ", ylabel = " Eig 4 " )
 
-eig_5 = Plots.plot(eig_hold[:,1], eig_hold[:,6], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Omnivory ", ylabel = " Eig 5 " )
+eig_5 = Plots.plot(eig_hold[:,1], eig_hold[:,6], legend = false, lw= 2.0, colour = "black", xlabel = " Strength of Coupling ", ylabel = " Eig 5 " )
 
 maxeig_hold = fill(0.0,length(coup_vals),2)
 
